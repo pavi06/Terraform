f735308594d51c901a942017a080fac8cd2add90
Pavithra P <pavithrap@presidio.com>
2024-12-19 14:19:31 +0530

    files added

---
+++ s3.tf
@@ -0,0 +1,19 @@
+#bucket creation
+resource "aws_s3_bucket" "paviS3" {
+  bucket = "pavi-bucket-from-githubaction"
+  tags = {
+    Name        = "pavi-bucket-from-githubaction"
+    Createdby   = "pavi"
+    Environment = "Dev"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "pavi-terraform-states-gha"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "pavi-terra-stlock"
+    encrypt = true
+  }
+}
